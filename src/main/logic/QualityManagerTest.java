fb273cd6bba74539c9b1b7b3b7004aaac8f224e3
norbix93 <norbix93@gmail.com>
2019-04-30 13:30:04 +0200

    optimize imports

--- src/main/logic/QualityManagerTest.java
+++ src/main/logic/QualityManagerTest.java
@@ -1,6 +1,5 @@
 package main.logic;
 
-import main.logic.QualityManager;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
